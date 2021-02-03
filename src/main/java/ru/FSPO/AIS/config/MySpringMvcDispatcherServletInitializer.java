67359a8df6ce40d79a8b8a562a4ab701e6f4ac71
VanDerLindeDutch <sotn.daniil@yandex.ru>
2021-02-03 12:15:25 +0300

    AIS

---
+++ src/main/java/ru/FSPO/AIS/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,21 @@
+package ru.FSPO.AIS.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
